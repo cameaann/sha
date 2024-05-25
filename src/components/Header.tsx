fc8d4d816dadb3babb1f58cab55283bec0c6ca24
Anna <cameaann@gmail.com>
2024-05-25 18:21:52 +0300

    adding header, added projects-db.json

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () =>{
+
+	return(
+		<header>
+			<ul className="nav">
+				<li className="nav-item"><a href="#">About me</a></li>
+				<li className="nav-item"><a href="">My projects</a></li>
+				<li className="nav-item"><a href="">My skills</a></li>
+				<li className="nav-item"><a href="">Contact</a></li>
+			</ul>
+		</header>
+	)
+}
+export default Header;
