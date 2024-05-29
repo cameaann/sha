1e569a7a6bb6581fdcce4303018fcb4f73d3c9eb
Anna <cameaann@gmail.com>
2024-05-29 11:47:16 +0300

    testing img url

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,7 +5,7 @@ const Project = ({project} : {project: TProject}) => {
 	return(
 		<a href={project.src} className="project" target="blank">
 			<figure className="project__img">
-				<img src={project.iconUrl} alt="project image" />
+				<img src= {project.iconUrl} alt="project image" />
 			</figure>
 			<div className="project__content">
 				<h3 className="project__title">{project.description}</h3>
