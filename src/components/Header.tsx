475e0d0498ddeebb9117df56ee1f675d5d8726e7
Anna <cameaann@gmail.com>
2024-05-29 11:28:44 +0300

    adding about, skills, contact, footer components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,13 @@ const Header = () =>{
 
 	return(
 		<header>
+			<div className="logobox">
+				<figure><img src="" alt="logo" className="logo" /></figure>
+				<h2 className="logo-name">Anna Shestakova</h2>
+			</div>
 			<ul className="nav">
-				<li className="nav-item"><a href="#">About me</a></li>
 				<li className="nav-item"><a href="">My projects</a></li>
+				<li className="nav-item"><a href="#">About me</a></li>
 				<li className="nav-item"><a href="">My skills</a></li>
 				<li className="nav-item"><a href="">Contact</a></li>
 			</ul>
