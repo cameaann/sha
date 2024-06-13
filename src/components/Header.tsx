79fd50c5ddd8133b5742b99e01bc4130cc87260f
Anna <cameaann@gmail.com>
2024-06-13 18:14:12 +0300

    adding close btn to mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,46 @@
 import { useState } from "react";
 
-const Header = () =>{
-const [showMobileView, setShowMobileView] = useState(false);
 
-const toggleMenu: React.MouseEventHandler<HTMLDivElement> = (event) =>{
-	event.preventDefault();
-	setShowMobileView(!showMobileView);
-}
-	return(
-		<header>
-			<div className="logobox">
-				<figure><img src="/sha/img/logo.svg" alt="logo" className="logo" /></figure>
-			</div>
-			<ul className={ showMobileView ? 'mobile' : 'nav'}>
-				<li className="nav-item"><a href="#about">About me</a></li>
-				<li className="nav-item"><a href="#projects">My projects</a></li>
-				<li className="nav-item"><a href="#skills">My skills</a></li>
-				<li className="nav-item"><a href="#contact">Contact</a></li>
-			</ul>
-			<div className= "mobile-menu-btn" onClick={toggleMenu}>
-			  <i className="fa-solid fa-bars"></i>
-			</div>
-		</header>
-	)
-}
+const Header = () => {
+  const [showMobileView, setShowMobileView] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const toggleMenu: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    event.preventDefault();
+    setShowMobileView(!showMobileView);
+	setMenuIsOpen(!menuIsOpen);
+  };
+  const closeMobileMenu = () => {
+    // if (isMobile) {
+	  setShowMobileView(false);
+      setMenuIsOpen(false);
+    // }
+  };
+  return (
+    <header>
+      <div className="logobox">
+        <figure>
+          <img src="/sha/img/logo.svg" alt="logo" className="logo" />
+        </figure>
+      </div>
+      <ul className={showMobileView ? "mobile" : "nav"}>
+        <li className="nav-item" onClick={closeMobileMenu}>
+          <a href="#about">About me</a>
+        </li>
+        <li className="nav-item" onClick={closeMobileMenu}>
+          <a href="#projects">My projects</a>
+        </li>
+        <li className="nav-item" onClick={closeMobileMenu}>
+          <a href="#skills">My skills</a>
+        </li>
+        <li className="nav-item" onClick={closeMobileMenu}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="mobile-menu-btn" onClick={toggleMenu}>
+			<i className={menuIsOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
+      </div>
+    </header>
+  );
+};
 export default Header;
